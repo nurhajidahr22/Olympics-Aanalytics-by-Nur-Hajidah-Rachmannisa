b8dbfa3eb69d70605716661ea62df516cd6e1b86
nurhajidahr22 <nurhajidahrachmannisa@gmail.com>
2024-08-20 17:42:52 +0700

    Delete 1.sql

--- 1.sql
+++
@@ -1,8 +0,0 @@
-SELECT athlete,
-  sport,
-  COUNT(medal) medals,
-  STRING_AGG(edition, ', ' ) followed_edition
-FROM portfolio-431701.Olimpiade.Event_Result_Cleaned R
-WHERE country_noc = "INA"
-GROUP BY 1,2
-ORDER BY 3 DESC
